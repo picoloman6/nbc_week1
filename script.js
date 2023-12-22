d62d18da01dce293d2102d94542faadb1cbfa8a7
박서영 <seokachuu@gmail.com>
2023-12-22 16:28:13 +0900

    카드 이미지 조정

--- script.js
+++ script.js
@@ -9,17 +9,18 @@ $('#inputbtn').click(function () {
   let mbti = $('.inputmbti').val()
   let tmi = $('.inputtmi').val()
 
-  let temp_html = `<div class="col">
-  <div class="card">
-    <img src="${photo}" class="card-img-top" alt="..." />
-    <div class="card-body">
-      <h5 class="card-title">${name}</h5>
-      <p class="card-text">${mbti}
-      </p>
-      <p class="card-text">${tmi}
-      </p>
+  let temp_html = `
+  <div class="col card-list">
+    <div class="card">
+      <img src="${photo}" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">${mbti}
+        </p>
+        <p class="card-text">${tmi}
+        </p>
+      </div>
     </div>
-  </div>
 </div>`
 $('#card').append(temp_html);
 });
