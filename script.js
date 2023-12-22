e35545fc365a45e44721af1ae2d76891104965ea
HuaHuaChiChi <seunghuigim995@gmail.com>
2023-12-22 12:46:45 +0900

    카드 내용추가기능

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 $("#addButton").click(function () {
   $(".input-form").toggle();
 });
+
+$('#inputbtn').click(function () {
+
+  let photo = $('.inputphoto').val()
+  let name = $('.inputname').val()
+  let mbti = $('.inputmbti').val()
+  let tmi = $('.inputtmi').val()
+
+  let temp_html = `<div class="col">
+  <div class="card">
+    <img src="${photo}" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">${name}</h5>
+      <p class="card-text">${mbti}
+      </p>
+      <p class="card-text">${tmi}
+      </p>
+    </div>
+  </div>
+</div>`
+$('#card').append(temp_html);
+});
