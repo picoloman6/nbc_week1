9bb4464f9de2d5ff74a3c27ff6778d5f27552870
picoloman6 <a2328b@naver.com>
2023-12-26 10:40:24 +0900

    방명록 페이지, js 파일 생성

---
+++ script2.js
@@ -0,0 +1,26 @@
+// Firebase SDK 라이브러리 가져오기
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+// Firebase 구성 정보 설정
+const firebaseConfig = {
+  apiKey: "AIzaSyBe-pgwVvrcspoZvg_qX6QSxgxljHh3t3M",
+  authDomain: "nbcweek1-79c87.firebaseapp.com",
+  projectId: "nbcweek1-79c87",
+  storageBucket: "nbcweek1-79c87.appspot.com",
+  messagingSenderId: "232350391237",
+  appId: "1:232350391237:web:f27f6fc9ef79995e7049ac",
+  measurementId: "G-60GQBCJ2Z0",
+};
+
+// Firebase 인스턴스 초기화
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
