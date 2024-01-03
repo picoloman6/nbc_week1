12b62afb54e8682225d71cf79f1c37a4048f1e3f
picoloman6 <a2328b@naver.com>
2024-01-03 09:38:45 +0900

    환경변수로 변경

--- script.js
+++ script.js
@@ -20,13 +20,13 @@ import {
 
 // Firebase 구성 정보 설정
 const firebaseConfig = {
-  apiKey: "AIzaSyBe-pgwVvrcspoZvg_qX6QSxgxljHh3t3M",
-  authDomain: "nbcweek1-79c87.firebaseapp.com",
-  projectId: "nbcweek1-79c87",
-  storageBucket: "nbcweek1-79c87.appspot.com",
-  messagingSenderId: "232350391237",
-  appId: "1:232350391237:web:f27f6fc9ef79995e7049ac",
-  measurementId: "G-60GQBCJ2Z0",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
 };
 
 // Firebase 인스턴스 초기화
